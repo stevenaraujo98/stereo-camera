e644a6dfb0676ed2423d6c327013f6c4aee2b79c
Steven Araujo <steven.araujo@hotmail.com>
2024-02-19 16:00:42 -0500

    button return

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -17,22 +17,26 @@ class App(tk.Tk):
         self.__launch_dialog()
 
     def __launch_dialog(self):
+        self.first_panel = tk.Frame(self, width=640, height=480, padx=10)
+        self.first_panel.pack(expand=True, anchor=tk.N)
+
         # Textos
-        self.texto1 = tk.Label(self, text="Configuración de cámara", font=16)
+        self.texto1 = tk.Label(self.first_panel, text="Configuración de cámara", font=16)
         self.texto1.grid(row=0, column=0, columnspan=2, padx=5, pady=10)
 
-        self.texto2 = tk.Label(self, text="Resolución:", font=12)
+        self.texto2 = tk.Label(self.first_panel, text="Resolución:", font=12)
         self.texto2.grid(row=1, column=0, padx=5, pady=5, sticky="e")
 
-        self.combo = ttk.Combobox(self, state="readonly", values=["1920x1080", "1280×720", "800x600", "640x480", "320x240"], font=12)
+        self.combo = ttk.Combobox(self.first_panel, state="readonly", values=["1920x1080", "1280x720", "800x600", "640x480", "320x240"], font=12)
         self.combo.set("1920x1080")
         self.combo.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
-        self.checkbox_value = tk.BooleanVar(self)
-        self.checkbox = tk.Checkbutton(self, text="Grabar video", variable=self.checkbox_value, command=self.checkbox_clicked, font=12, padx=0)
+        self.checkbox_value = tk.BooleanVar(self.first_panel)
+        self.checkbox = tk.Checkbutton(self.first_panel, text="Grabar video", variable=self.checkbox_value, command=self.checkbox_clicked, font=12, padx=0)
         self.checkbox.grid(row=2, column=0, columnspan=2, pady=5, sticky="w")
 
         self.button = tk.Button(
+            self.first_panel,
             text="Aceptar",
             cursor="hand2",
             command=self.start_camera,
@@ -43,23 +47,18 @@ class App(tk.Tk):
     def checkbox_clicked(self):
         print(self.checkbox_value.get())
     
-    def start_camera(self):
+    def start_camera(self):        
         list_size = self.combo.get().split("x")
         camera_width = int(list_size[0])
         camera_height = int(list_size[1])
         print(camera_width, camera_height)
 
         # Ocultar los widgets de configuración
-        self.texto1.grid_forget()
-        self.texto2.grid_forget()
-        self.combo.grid_forget()
-        self.checkbox.grid_forget()
-        self.button.grid_forget()
-
+        self.first_panel.destroy()
 
         self.center_panel = tk.Frame(
             self, width=640, height=480, padx=10)
-        self.center_panel.pack(side="left", fill="both", expand=True, anchor=tk.N)
+        self.center_panel.pack(expand=True, anchor=tk.N)
 
         # Mostrar la visualización de la cámara
         self.vid = cv2.VideoCapture(0)
@@ -105,21 +104,18 @@ class App(tk.Tk):
             self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
             self.canvas_2.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
-        self.after(10, self.update_camera) #10 milisegundos
+        self.after_id = self.after(10, self.update_camera) #10 milisegundos
 
     def return_to_config(self):
         # Ocultar los widgets de configuración
-        # self.canvas.grid_forget()
-        # self.canvas.destroy()
-        # self.canvas_2.grid_forget()
-        # self.canvas_2.destroy()
-        # self.btn_snapshot.grid_forget()
-        # self.return_button.grid_forget()
         self.center_panel.destroy()
         
         # Detener la visualización de la cámara
         if self.vid and self.vid.isOpened():
             self.vid.release()
+        
+        if self.after_id:
+            self.after_cancel(self.after_id)
 
         # Volver a mostrar los widgets de configuración
         self.__launch_dialog()
